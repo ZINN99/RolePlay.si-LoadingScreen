3224ff42f54d22854c3a3f3012f662a4a4f8d1dd
jostek_ <132101240+Jostekkk@users.noreply.github.com>
2023-06-25 08:25:23 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+games { 'common' }
+
+author { 'Jost' }
+description { 'Jostov fivem loadingscreen' }
+version '0.2.0'
+
+loadscreen 'loadscreen.html'
+
+files {
+    'loadscreen.html',
+    'style.css',
+    'music.mp3',
+}
